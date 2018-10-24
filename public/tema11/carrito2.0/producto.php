05fc4be5b650aaff182c3cf2725adc623019976a
Unknown <eduarmarso@hotmail.com>
2018-10-24 18:53:01 +0200

    Tema 11 - Carrito2.0

---
+++ public/tema11/carrito2.0/producto.php
@@ -0,0 +1,48 @@
+<?php 
+
+class Producto
+{
+
+	private $nombre;
+	private $precio;
+	private $iva;
+	private $cantidad = 1;
+
+	public function mostrar()
+	{
+		return "<p>{$this->nombre}: $this->precio &euro; + {$this->iva}%</p>";
+	}
+	
+	public function precio()
+	{
+		return $this->precio;
+	}
+
+	public function precioIva()
+	{
+		return round($this->precio + ($this->precio * $this->iva / 100), 2);
+	}
+
+	public function __construct($nombre, $precio, $iva=21)
+	{
+		$this->nombre = $nombre;
+		$this->precio = $precio;
+		$this->iva = $iva;
+	}
+
+	public function masUnidad($unidades = 1)
+	{
+		$this->cantidad += $unidades;
+	}
+
+	public function menosUnidad()
+	{
+		if ($this->cantidad > 0) {
+			$this->cantidad--;
+		}
+	}
+
+}
+
+
+?>
